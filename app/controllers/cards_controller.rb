54db173d03a7ebbdc46a3b75f92fadeea29b3a47
Pavel Pestau <pestovpvl@mail.ru>
2023-06-09 08:51:25 -0400

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   before_action :set_card, only: %i[ show edit update destroy ]
+  before_action :require_owner, only: %i[ show edit update destroy ]
 
   # GET /cards or /cards.json
   def index
@@ -68,5 +69,11 @@ class CardsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def card_params
       params.require(:card).permit(:word, :definition, :leitner_card_box_id)
+    end
+
+    def require_owner
+      unless current_user == @card.user
+        redirect_to root_url, notice: "You are not authorized to perform this action."
+      end
     end
 end
