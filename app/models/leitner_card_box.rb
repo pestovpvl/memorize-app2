646a13037d4c9f4a1d1bc15c84821ec2fb2ec8c0
Pavel Pestau <pestovpvl@mail.ru>
2023-06-10 20:41:05 -0400

    Move default to model

--- app/models/leitner_card_box.rb
+++ app/models/leitner_card_box.rb
@@ -2,6 +2,11 @@ class LeitnerCardBox < ApplicationRecord
   belongs_to :user
   has_many :cards
 
+  validates :repeat_period, presence: true, numericality: { only_integer: true, greater_than: 0 }, uniqueness: { scope: :user_id }
+
+  # default repeat periods for new users
+  DEFAULT_REPEAT_PERIODS = [1, 3, 7].freeze
+  
   before_destroy :remove_associated_cards
 
   private
