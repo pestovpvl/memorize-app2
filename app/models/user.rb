d8a816c96e4fbf28cb1ccfaa8b88026afc322541
Pavel Pestau <pestovpvl@mail.ru>
2023-06-10 20:41:42 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :leitner_card_boxes, dependent: :destroy
   has_many :cards, dependent: :destroy
+
+  after_create :create_leitner_card_boxes
+
+  private
+
+  def create_leitner_card_boxes
+    LeitnerCardBox::DEFAULT_REPEAT_PERIODS.each do |repeat_period|
+      leitner_card_boxes.create!(repeat_period: repeat_period)
+    end
+  end
 end
