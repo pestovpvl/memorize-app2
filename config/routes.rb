6ca74e9bcb57ca64a836481bf418b01663c98d22
Pavel Pestau <pestovpvl@mail.ru>
2023-06-14 10:45:23 -0400

    Add learn actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :leitner_card_boxes
-  resources :cards
+  resources :cards do
+    collection do
+      get :learn
+    end
+    member do
+      post :remember
+      post :forget
+    end
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
